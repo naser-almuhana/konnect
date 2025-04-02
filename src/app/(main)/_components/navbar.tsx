c3b012ec4fc61aa4dd5adbe726c3e1db7d71c17a
naseralmuhana <naseralmuhana0@gmail.com>
2025-04-02 07:01:21 +0300

    add layout

---
+++ src/app/(main)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+  return (
+    <header className="bg-card sticky top-0 z-10 shadow-sm">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+        Navbar
+      </div>
+    </header>
+  )
+}
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation"
 
 import { validateRequest } from "@/lib/auth"
 
+import { Navbar } from "./_components/navbar"
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -11,5 +13,16 @@ export default async function RootLayout({
 
   if (user) redirect("/")
 
-  return <>{children}</>
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+      <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+        {/* <MenuBar className="bg-card sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" /> */}
+        MenuBar
+        {children}
+      </div>
+      {/* <MenuBar className="bg-card sticky bottom-0 flex w-full justify-center gap-5 border-t p-3 sm:hidden" /> */}
+      MenuBar
+    </div>
+  )
 }
